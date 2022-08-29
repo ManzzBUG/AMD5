66fdb416d7acb0cc87e49ab48672fd87529586aa
DHAFF OFFICIAL <103863637+Dhaffnavyz@users.noreply.github.com>
2022-08-29 16:15:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# mini-games-html-css-js-basic
+
+this code is just for education and entertainment 👌
+
+tutorial on my youtube:
+https://youtube.com/channel/UCIfD_R1Jph8MUe0ZbwKCstQ
+ <div> far (dhaffbot)run,/akses/poin,/player.
---
+++ config.js
@@ -0,0 +1,128 @@
+//════════════════════════════//
+
+const fs = require("fs")
+const chalk = require("chalk")
+
+//aumto functioner
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
+global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['6283824417034','6285797226828'] //masukin no owner nya
+global.ownername = "𝙼𝚁. 𝙳𝙷𝙰𝙵𝙵 ¹¹¹⁰¹"
+global.ytname = "YT: DHAFF OFFICIAL" //ur yt chanel name
+global.socialm = "GitHub: Dhaffnavyz" //ur github or insta name
+global.location = "Indonesia, Jawabarat, Rancaekek" //ur location
+
+//bot bombdy
+global.botname = "DaisuXe-Md" //ur bot name
+global.ytwebsite = "https://youtu.be/JcXWXjdG9hI"
+global.websitex = "https://bit.ly/Dhaffwebsite_my-id" //ur website
+global.vidmenu = { url: 'https://k.top4top.io/m_2368l5agg1.mp4' } //gif and video menu
+global.packname = "DasuXe Bot Stickers\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n🕊️🕊️ "
+global.author = "DHAFF OFFICIAL" 
+global.themeemoji = "🕊️"
+global.reactmoji = "🐣"
+global.ownertag = ['6283824417034','6285797226828'] //ur owner tag
+global.ownernummenu = ['6285797226828','6283824417034'] //ur owner number in menu and all
+global.watermark = "DaisuXe-MDv3." //ur watermark
+global.botscript = "https://github.com/Dhaffnavyz/BaseBotMd-Origin"
+global.linkz1 = "https://chat.whatsapp.com/GToAFWm7W6g9noPQi08Qba"
+global.linkz2 = "https://chat.whatsapp.com/KCMvdghiofF0R1HlgoGAew"
+
+//Bot theme media
+global.thum = fs.readFileSync("./Galery/theme/DNP77picture.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./Galery/theme/DNP77picture.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./Galery/theme/DNP77picture.jpg") //ur error pic
+global.thumb = fs.readFileSync("./Galery/theme/DNP77picture.jpg") //ur thumb pic
+
+//database
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//omther
+global.lolhuman = "KaysaS"
+global.sessionName = "sessionnya"
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.mess = {
+    success: 'Done✓',
+    admin: 'This feature is only for admins!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This feature is only for owner',
+    group: 'This feature is only for groups!',
+    private: 'This feature is only for private chats!',
+    bot: 'This feature is only for bot',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    error: 'Error!',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+// api website by xenz
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// key
+global.APIKeys = {
+	'https://zenzapis.xyz': 'Your Key',
+}
+//menu logo maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
